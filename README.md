e151bb0defedc0afeaf9475e91361ad8e1491873
Arunas Skirius <arukomp@gmail.com>
2016-07-05 09:49:18 +0100

    #8 Completed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # FizzBuzz kata
 
-Completed `7` times
+Completed `8` times
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,10 +1,26 @@
 require 'fizzbuzz'
 
-describe "fizzbuzz" do
-  carousel = ['fizzbuzz', nil, nil, 'fizz', nil, 'buzz', 'fizz', nil, nil, 'fizz', 'buzz', nil, 'fizz', nil, nil]
-  (1..1000).each do |n|
-    it "should return appropriate value for a given number" do
-      expect(fizzbuzz(n)).to eq (carousel[n%15] ? carousel[n%15] : n)
-    end
+describe "FizzBuzz" do
+
+  it "returns 'fizz' when given numbers 3, 6, 9, etc" do
+    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(6)).to eq 'fizz'
+    expect(fizzbuzz(9)).to eq 'fizz'
   end
+  it "returns 'buzz' when given numbers 5, 10, 20, etc" do
+    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz(10)).to eq 'buzz'
+    expect(fizzbuzz(20)).to eq 'buzz'
+  end
+  it "returns 'fizzbuzz' when given numbers 15, 30, 60, etc" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+    expect(fizzbuzz(60)).to eq 'fizzbuzz'
+  end
+  it "returns the number itself when given numbers 2, 4, 8, etc" do
+    expect(fizzbuzz(2)).to eq 2
+    expect(fizzbuzz(4)).to eq 4
+    expect(fizzbuzz(8)).to eq 8
+  end
+
 end
